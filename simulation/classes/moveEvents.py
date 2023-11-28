5d42c76ca9afd992d3ad373c5f8223352c17bb99
becauselol <63877847+becauselol@users.noreply.github.com>
2023-11-29 00:13:19 +0800

    feat: updated the logic flow for events in description

--- simulation/classes/moveEvents.py
+++ simulation/classes/moveEvents.py
@@ -7,24 +7,50 @@ NOTE: time is the time that the event occurs
 
 # Sequence of events when it reaches the correct floor
 # CONDITION: elevator is moving in direction up
+0. Elevator reaches the floor
+    What this means is that we eliminate any internal calls and any external calls
+
+    DOOR OPEN --> triggers ALIGHT if alighting, triggers BOARD if boarding, triggers DOOR CLOSE
+
 1. Check if there is anyone alighting
+
+    ALIGHT
+
 2. Alight people
 3. wait for a bit
 4. check if there is anyone who wants to board in the direction up
+
+    BOARD
+
 5. if yes, board them
+    update the internal calls
+
+    DOOR CLOSE --> trigger DOOR OPEN if change direction, trigger MOVE TO IDLE if wait, trigger NEXT FLOOR if no change
+
 6. check if we need to change direction
     7. check if there are any people in the lift who still want to go up
     8. check if there are any calls above it that need to respond to
 9. if no need to change direction
-10. carry on 
+10. carry on    NEXT FLOOR
 
 11. check if there are any calls below this floor as well (this includes anyone who wants to board on this floor)
+
+    DOOR OPEN --> trigger BOARD if boarding else DOOR CLOSE
+
 12. if yes, change direction
+    This then means we eliminate the external call that is to go down
 13. check if there is anyone who wants to board in the direction down
 14. if yes, board them
-14. carry on
+    update the internal calls
+
+    DOOR CLOSE --> trigger DOOR OPEN if change direction, trigger MOVE TO IDLE if wait, trigger NEXT FLOOR if no change
+
+14. carry on    NEXT FLOOR
 
 15. If no then just change state to WAIT 
+
+    MOVE TO IDLE
+
 16. And also add event to trigger the movement to Idle
 
 """
