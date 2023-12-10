c549a5b8bc6978dc8c4071b5f56cbae214d8bd4c
kaixinchloe260 <kaixinchloe260@gmail.com>
2023-12-10 16:17:48 +0800

    graph output

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
 import numpy as np
 import random
+import pandas as pd
 from simulation.classes.sim import Simulation
 import simulation.classes.groupController as gControl
 
-from analysis.homogeneous.analysis import print_res, convert_to_reward, calculate_expected_reward
+from analysis.homogeneous.analysis import print_res, convert_to_reward, calculate_expected_reward, plt_graph
 
 if __name__ == "__main__":
     print("INITIALIZING SIMULATION\n")
@@ -46,6 +47,8 @@ if __name__ == "__main__":
     print(f"\nSTARTING SIMULATION\n")
     
     simulation_data = []
+    result_wait_t = []
+    result_idle_t = []
 
     for idx, cycle_data in enumerate(sim.simulate(simulation_duration)):
         # print("cycle:", idx)
@@ -62,7 +65,11 @@ if __name__ == "__main__":
     for floor, rewards in reward_dict["wait_time"].items():
         num_passengers = reward_dict["num_passenger"][floor]
         result = calculate_expected_reward(num_passengers, rewards)
+        result["floor"] = floor
+        result_wait_t.append(result)
         print_res(result, f"wait time for floor {floor}")
+
+    wait_time_data = pd.DataFrame(result_wait_t)
 
     for floor, rewards in reward_dict["num_passenger"].items():
         result = calculate_expected_reward(cycle_len_arr, rewards)
@@ -70,4 +77,12 @@ if __name__ == "__main__":
 
     for elevator_id, rewards in reward_dict["idle_time"].items():
         result = calculate_expected_reward(cycle_len_arr, rewards)
+        result["elevator"] = elevator_id
+        result_idle_t.append(result)
         print_res(result, f"idle time for elevator {elevator_id}")
+    idle_time_data = pd.DataFrame(result_idle_t)
+
+    print(idle_time_data)
+    
+    plt_graph(wait_time_data, "wait_time")
+    plt_graph(idle_time_data, "idle_time")
